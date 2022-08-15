6727722382da048f5c2dd89c699b7baeb32e9837
kaarthiksundar <kaarthik01sundar@gmail.com>
2022-08-15 14:14:44 -0600

    adds steady sim data

--- julia-src/main.jl
+++ julia-src/main.jl
@@ -17,6 +17,10 @@ function parse_cli_args(args)
             arg_type = String
             default = "./json/"
             help = "folder to save the output json"
+        "--writebc"
+            arg_type = Int 
+            default = 0
+            help = "0/1 to write boundary condition files for steady state simulation"
     end
     return parse_args(s) # the result is a Dict{String,Any}
 end
@@ -24,7 +28,7 @@ end
 function main(ARGS)
     args = parse_cli_args(ARGS)
     data = parse_gaslib(args["datafolder"] * args["file"])
-    write_network_data(data, args["outputfolder"], args["file"]; bc_flag=true)
+    write_network_data(data, args["outputfolder"], args["file"]; bc_flag=Bool(args["writebc"]))
     write_params_data(data, args["outputfolder"], args["file"])
     write_decision_group_data(data, args["outputfolder"], args["file"])
     write_nomination_data(data, args["outputfolder"], args["file"])
