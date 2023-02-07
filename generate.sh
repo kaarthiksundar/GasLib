b01dd1910680a6136a07d7cf79133317ee9582f6
kaarthiksundar <kaarthik01sundar@gmail.com>
2023-02-07 16:08:23 -0700

    updates data

---
+++ generate.sh
@@ -0,0 +1,7 @@
+julia --project=. julia-src/main.jl --file GasLib-11.zip
+julia --project=. julia-src/main.jl --file GasLib-24.zip
+julia --project=. julia-src/main.jl --file GasLib-40.zip
+julia --project=. julia-src/main.jl --file GasLib-134.zip
+julia --project=. julia-src/main.jl --file GasLib-135.zip
+julia --project=. julia-src/main.jl --file GasLib-582.zip
+julia --project=. julia-src/main.jl --file GasLib-4197.zip
---
+++ json/create_zip.sh
@@ -0,0 +1,7 @@
+zip -r GasLib-11.zip GasLib-11
+zip -r GasLib-24.zip GasLib-24
+zip -r GasLib-40.zip GasLib-40
+zip -r GasLib-134.zip GasLib-134
+zip -r GasLib-135.zip GasLib-135
+zip -r GasLib-582.zip GasLib-582
+zip -r GasLib-4197.zip GasLib-4197
